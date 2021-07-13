82a252972db0ddd18970e26b2a8709c34b996352
juan gabriel rios <juangrios27@gmail.com>
2021-07-12 22:11:28 -0300

    agregue eventos y modifique el DOM-clase 8

--- app.js
+++ app.js
@@ -4,83 +4,36 @@ let cantidadDeCajas = 0;
 let precioPegamento = 1200;
 let precioPastina = 150;
 let modelo = '';
-
-// datos que ingresa el cliente
-let orden = Number(
-	prompt(
-		'Elija el orden en que se mostraran los porcelanato pór consola: de menor a mayor precio presione 1 ; de mayor a menor presione 2'
-	)
-);
-let numModelo = Number(
-	prompt(
-		'Elija un porcelanato: para Carrara 60x120 presione 1 ; para Onix 60x60 presione 2 ; para Bauhaus 58x58 presione 3'
-	)
-);
-let metrosCuadrados = Number(prompt('Ingrese la cantidad de metros cuadrados a cubrir'));
-
-// validacion
-while (orden != 1 && orden != 2) {
-	alert('Usted a ingresado un numero de ordenamiento incorrecto');
-	orden = Number(
-		prompt(
-			'Elija el orden en que se mostraran los porcelanato pór consola: de menor a mayor precio escriba 1 ; de mayor a menor precio escriba 2'
-		)
-	);
-}
-while (numModelo != 1 && numModelo != 2 && numModelo != 3) {
-	alert('Usted a ingresado un numero de modelo incorrecto');
-	numModelo = Number(
-		prompt(
-			'Elija un porcelanato: para Carrara 60x120 presione 1 ; para Onix 60x60 presione 2 ; para Bauhaus 58x58 presione 3'
-		)
-	);
-}
-while (metrosCuadrados <= 0 || !metrosCuadrados) {
-	alert('Usted a ingresado una cantidad de metros cuadrado incorrecta');
-	metrosCuadrados = Number(prompt('Ingrese la cantidad de metros cuadrados a cubrir'));
-}
+let orden = 1; //para que los porcelanatos se muestren en orden por precio en la consola: 1-ascendente; 2-descendente
+let verificador1 = true;
+let verificador2 = true;
 
 // clase para productos
 class producto {
-	constructor(nombre, medida, caja, precio, imagen) {
+	constructor(codigo, nombre, medida, caja, precio, imagen) {
+		this.codigo = codigo;
 		this.nombre = nombre;
 		this.medida = medida;
 		this.caja = caja;
 		this.precio = precio;
 		this.imagen = imagen;
 	}
-	foto() {
-		document.write(
-			`<h1>${modelo.nombre}</h1><img src="imagenes/${this.imagen}" alt="porcelanato ${this.nombre}">`
-		);
-	}
 }
 
 // carga de productos - creacion de objetos
-const carrara = new producto('Carrara', '60x120', 1.44, 5000, 'carrara.jpg');
-const onix = new producto('Onix', '60x60', 1.44, 4200, 'onix.jpg');
-const bauhaus = new producto('Bauhaus', '58x58', 1.35, 2800, 'bauhaus.jpg');
+const carrara = new producto(1, 'Carrara', '60x120', 1.44, 5000, 'carrara.jpg');
+const onix = new producto(2, 'Onix', '60x60', 1.44, 4200, 'onix.jpg');
+const bauhaus = new producto(3, 'Bauhaus', '58x58', 1.35, 2800, 'bauhaus.jpg');
 // cargo mis productos en el array
 const porcelanatos = [carrara, onix, bauhaus];
-// cargo el modelo elegido
-switch (numModelo) {
-	case 1:
-		modelo = carrara;
-		break;
-	case 2:
-		modelo = onix;
-		break;
-	case 3:
-		modelo = bauhaus;
-		break;
-}
 
 // funciones
+
 function ascendente(a, b) {
-	return a.precio - b.precio;
+	return a.codigo - b.codigo;
 }
 function descendente(a, b) {
-	return b.precio - a.precio;
+	return b.codigo - a.codigo;
 }
 function cantidad(metrosCuadrados) {
 	cantidadDeCajas = Math.ceil(metrosCuadrados / modelo.caja);
@@ -109,13 +62,62 @@ let precioTotalPastina = (precioPastina, cantPegamentopastina) => {
 function precioFinal(preciopiso, precioTotalPegamento, precioTotalPastina) {
 	return preciopiso + precioTotalPegamento + precioTotalPastina;
 }
-
+function mostrar() {
+	let numModelo = Number(document.getElementById('codigo').value);
+	let metrosCuadrados = Number(document.getElementById('cantidad').value);
+	if (numModelo != 1 && numModelo != 2 && numModelo != 3) {
+		alert('Usted a ingresado un numero de modelo incorrecto');
+		verificador1 = false;
+	} else {
+		verificador1 = true;
+	}
+	if (metrosCuadrados <= 0 || !metrosCuadrados) {
+		alert('Usted a ingresado una cantidad de metros cuadrado incorrecta');
+		verificador2 = false;
+	} else {
+		verificador2 = true;
+	}
+	if (verificador1 && verificador2) {
+		switch (numModelo) {
+			case 1:
+				modelo = carrara;
+				break;
+			case 2:
+				modelo = onix;
+				break;
+			case 3:
+				modelo = bauhaus;
+				break;
+		}
+		let cantidadReal = cantidad(metrosCuadrados).toFixed(2);
+		let preciopiso = precioPorcelanato(cantidadReal);
+		let cantPegamentopastina = cantidadPegamentoPastina(cantidadReal);
+		let precioTotal = precioFinal(
+			preciopiso,
+			precioTotalPegamento(precioPegamento, cantPegamentopastina),
+			precioTotalPastina(precioPastina, cantPegamentopastina)
+		);
+		// modifico el DOM
+		let presupuesto = document.getElementById('presupuesto');
+		presupuesto.innerHTML = '';
+		let parrafo = document.createElement('p');
+		parrafo.textContent = `Por ${cantidadReal}m2 (${cantidadDeCajas} cajas) de porcelanato ${modelo.nombre} ${modelo.medida} + ${cantPegamentopastina} bolsas de pegamento de 30kg + ${cantPegamentopastina} bolsas de pastina de 1kg,  el precio total es de : $${precioTotal}`;
+		presupuesto.appendChild(parrafo);
+		parrafo.setAttribute('class', 'presupuesto__info');
+		let img = document.createElement('img');
+		presupuesto.appendChild(img);
+		img.setAttribute('src', `imagenes/${modelo.imagen}`);
+		img.setAttribute('class', 'presupuesto__img');
+	}
+}
 // ordeno los productos de menor a mayor precio y los muestro por consola
 console.log('Porcelanatos:');
 if (orden == 1) {
 	const ordenAscendente = porcelanatos.sort(ascendente);
 	for (const producto of ordenAscendente) {
-		console.log(`${producto.nombre} ${producto.medida}: $${producto.precio}`);
+		console.log(
+			`Codigo: ${producto.codigo} - ${producto.nombre} ${producto.medida}: $${producto.precio}`
+		);
 	}
 } else {
 	const ordenDescendente = porcelanatos.sort(descendente);
@@ -123,19 +125,9 @@ if (orden == 1) {
 		console.log(`${producto.nombre} ${producto.medida}: $${producto.precio}`);
 	}
 }
-// procesamiento de datos-llamo a las funciones
-let cantidadReal = cantidad(metrosCuadrados).toFixed(2);
-let preciopiso = precioPorcelanato(cantidadReal);
-let cantPegamentopastina = cantidadPegamentoPastina(cantidadReal);
-let precioTotal = precioFinal(
-	preciopiso,
-	precioTotalPegamento(precioPegamento, cantPegamentopastina),
-	precioTotalPastina(precioPastina, cantPegamentopastina)
-);
 
-// mensaje en pantalla
-alert(
-	`Por ${cantidadReal}m2 (${cantidadDeCajas} cajas) de porcelanato ${modelo.medida} + ${cantPegamentopastina} bolsas de pegamento de 30kg + ${cantPegamentopastina} bolsas de pastina de 1kg,  el precio total es de : $${precioTotal}`
-);
+// Agrego un evento
+
+let btnPre = document.getElementById('btnPre');
+btnPre.addEventListener('click', mostrar);
 // muestro foto del modelo elegido
-modelo.foto();
