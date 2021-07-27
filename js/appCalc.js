f4c6746499ca2a2252edd9775ea8f84411f04eac
juan gabriel rios <juangrios27@gmail.com>
2021-07-26 22:36:52 -0300

    pasado todo a jquery

--- js/appCalc.js
+++ js/appCalc.js
@@ -6,7 +6,7 @@ let precioPastina = 150;
 let modelo = '';
 let verificador1 = false;
 let verificador2 = true;
-
+let codViejo = 0;
 // base de datos de los porcelanatos
 const porcelanatos = JSON.parse(localStorage.getItem('porcelanatos'));
 console.log(porcelanatos);
@@ -15,7 +15,7 @@ const eleccion = JSON.parse(localStorage.getItem('eleccion'));
 console.log(eleccion);
 // si eleccion no es null cargo en el imput
 if (eleccion !== 0) {
-	document.getElementById('codigo').value = eleccion;
+	$('#codigo').val(eleccion);
 }
 // funciones
 function cantidad(metrosCuadrados) {
@@ -46,20 +46,34 @@ function precioFinal(preciopiso, precioTotalPegamento, precioTotalPastina) {
 	return preciopiso + precioTotalPegamento + precioTotalPastina;
 }
 function mostrar() {
-	let numModelo = Number(document.getElementById('codigo').value);
-	let metrosCuadrados = Number(document.getElementById('cantidad').value);
-	for (const value of porcelanatos) {
+	let numModelo = Number($('#codigo').val());
+	console.log(numModelo);
+	let metrosCuadrados = Number($('#cantidad').val());
+	let i = 0;
+	for (let value of porcelanatos) {
 		if (numModelo == value.codigo) {
 			modelo = value;
+			i++;
 			verificador1 = true;
+			console.log(modelo);
 		}
 	}
-	if (!verificador1) {
-		alert('Usted a ingresado un numero de modelo incorrecto');
+	if (i == 0) {
+		verificador1 = false;
 	}
-
+	console.log(verificador1);
+	$('.datos__alerta').remove();
+	if (!verificador1) {
+		$('.datos__box:first-child').append(
+			`<p class="datos__alerta">Usted a ingresado un numero de modelo incorrecto.</p>`
+		);
+		verificador1 = false;
+	}
+	console.log(verificador1);
 	if (metrosCuadrados <= 0 || !metrosCuadrados) {
-		alert('Usted a ingresado una cantidad de metros cuadrado incorrecta');
+		$('.datos__box:last').append(
+			`<p class="datos__alerta">Usted a ingresado una cantidad de metros cuadrado incorrecta.</p>`
+		);
 		verificador2 = false;
 	} else {
 		verificador2 = true;
@@ -72,77 +86,69 @@ function mostrar() {
 		let precioPast = precioTotalPastina(precioPastina, cantPegamentopastina);
 		let precioTotal = precioFinal(preciopiso, precioPeg, precioPast);
 		// modifico el DOM
-		let presupuesto = document.getElementById('presupuesto');
-		presupuesto.innerHTML = '';
-		let parrafo1 = document.createElement('p');
-		let parrafo2 = document.createElement('p');
-		let parrafo3 = document.createElement('p');
-		let parrafo4 = document.createElement('p');
-		parrafo1.textContent = `Por ${cantidadReal}m2 (${cantidadDeCajas} cajas) de porcelanato ${modelo.nombre} ${modelo.medida} el precio es de: $${preciopiso}`;
-		parrafo2.textContent = `Por ${cantPegamentopastina} bolsas de pegamento de 30kg el precio es de: $${precioPeg}`;
-		parrafo3.textContent = `Por ${cantPegamentopastina} bolsas de pastina de 1kg,  el precio es de: $${precioPast}`;
-		parrafo4.textContent = `El precio total es de : $${precioTotal}`;
-		presupuesto.appendChild(parrafo1);
-		presupuesto.appendChild(parrafo2);
-		presupuesto.appendChild(parrafo3);
-		presupuesto.appendChild(parrafo4);
-		parrafo1.setAttribute('class', 'presupuesto__info');
-		parrafo2.setAttribute('class', 'presupuesto__info');
-		parrafo3.setAttribute('class', 'presupuesto__info');
-		parrafo4.setAttribute('class', 'presupuesto__info');
-
-		let img = document.createElement('img');
-		presupuesto.appendChild(img);
-		img.setAttribute('src', `imagenes/${modelo.imagen}`);
-		img.setAttribute('class', 'presupuesto__img');
+		let presupuesto = $('.presupuesto');
+		$('.presupuesto__container').remove();
+		presupuesto.append(`
+			<div class="presupuesto__container">		
+				<p class="presupuesto__info">
+				Por ${cantidadReal}m2 (${cantidadDeCajas} cajas) de porcelanato ${modelo.nombre} ${modelo.medida} el precio es de: $${preciopiso}
+				</p>
+				<p class="presupuesto__info">Por ${cantPegamentopastina} bolsas de pegamento de 30kg el precio es de: $${precioPeg}</p>
+				<p class="presupuesto__info">Por ${cantPegamentopastina} bolsas de pastina de 1kg,  el precio es de: $${precioPast}</p>
+				<p class="presupuesto__info">El precio total es de : $${precioTotal}</p>
+				<img src="imagenes/${modelo.imagen}" class="presupuesto__img" />
+			</div>
+		`);
 		// cambio la seccion info
-		document.getElementById('porcNombre').textContent = `Porcelanato ${modelo.nombre}`;
-		document.getElementById('porcMedida').textContent = `Medida: ${modelo.medida}`;
-		document.getElementById(
-			'porcCaja'
-		).textContent = `Metros cuadrados por caja: ${modelo.caja} m2`;
-		document.getElementById('porcPrecio').textContent = `Precio: $${modelo.precio}`;
+		let info = $('.info');
+		info.append('');
+		info.append(`
+		<div class="presupuesto__container">
+			<h1 class="info__nombre" id="porcNombre">Porcelanato ${modelo.nombre}</h1>
+			<h4 class="info__detalle" id="porcMedida">Medida: ${modelo.medida}</h4>
+			<h4 class="info__detalle" id="porcCaja">Metros cuadrados por caja: ${modelo.caja} m2</h4>
+			<h4 class="info__detalle" id="porcPrecio">Precio x m2: $${modelo.precio}</h4>
+		</div>
+		`);
+	} else {
+		$('.presupuesto__container').remove();
 	}
 }
 function ingresaCod(value) {
 	for (modelo of porcelanatos) {
 		if (modelo.codigo == value) {
-			let presupuesto = document.getElementById('presupuesto');
-			let img = document.createElement('img');
-			presupuesto.appendChild(img);
-			img.setAttribute('src', `imagenes/${modelo.imagen}`);
-			img.setAttribute('class', 'presupuesto__img');
+			let presupuesto = $('.presupuesto');
+			$('.presupuesto__container').remove();
+			presupuesto.append(`
+				<div class="presupuesto__container">		
+					<img src="imagenes/${modelo.imagen}" class="presupuesto__img" />
+				</div>
+			`);
 			// cambio la seccion info
-			document.getElementById('porcNombre').textContent = `Porcelanato ${modelo.nombre}`;
-			document.getElementById('porcMedida').textContent = `Medida: ${modelo.medida}`;
-			document.getElementById(
-				'porcCaja'
-			).textContent = `Metros cuadrados por caja: ${modelo.caja} m2`;
-			document.getElementById('porcPrecio').textContent = `Precio: $${modelo.precio}`;
-			console.log(`este es el cambio, seleccion: ${modelo.nombre}`);
+			let info = $('.info');
+			info.append('');
+			info.append(`
+			<div class="presupuesto__container">
+				<h1 class="info__nombre" id="porcNombre">Porcelanato ${modelo.nombre}</h1>
+				<h4 class="info__detalle" id="porcMedida">Medida: ${modelo.medida}</h4>
+				<h4 class="info__detalle" id="porcCaja">Metros cuadrados por caja: ${modelo.caja} m2</h4>
+				<h4 class="info__detalle" id="porcPrecio">Precio x m2: $${modelo.precio}</h4>
+			</div>
+			`);
 		}
 	}
 }
 function cambio() {
-	let inputMod = document.getElementById('codigo');
-	inputMod.addEventListener('change', ingresaCod(inputMod.value));
+	let inputMod = $('#codigo');
+	console.log(`el codigo nuevo es ${inputMod.val()} y el viejo es ${codViejo}`);
+	if (codViejo != inputMod.val()) {
+		inputMod.on('change', ingresaCod(inputMod.val()));
+		codViejo = inputMod.val();
+	}
 }
 // observo el input para tomar cualquier cambio en tiempo real para mostrar el porcelanato elegido
-setInterval(cambio, 200000000);
+setInterval(cambio, 300);
 // calculo el presupuesto cuando se le da click al boton calcular
 
-let btnPre = document.getElementById('btnPre');
-btnPre.addEventListener('click', mostrar);
-
-// $('body').append(`<input type="text"   class="inputsClass">
-//                    <input type="number" class="inputsClass">
-//                    <select class="inputsClass">
-//                         <option value="1" selected >ID 1</option>
-//                         <option value="2">ID 2</option>
-//                         <option value="3">ID 3</option>
-//                     </select>`);
-// //Asociamos el evento change a todos los inputs
-// $('.inputsClass').change(function () {
-// 	// console.log(e.target.value);
-// 	console.log(this.value);
-// });
+let btnPre = $('#btnPre');
+btnPre.on('click', mostrar);
--- js/appPre.js
+++ js/appPre.js
@@ -4,56 +4,20 @@ let cantidadDeCajas = 0;
 let precioPegamento = 1200;
 let precioPastina = 150;
 let modelo = '';
-let orden = 1; //para que los porcelanatos se muestren en orden por precio en la consola: 1-ascendente; 2-descendente
-let verificador1 = true;
+let verificador1 = false;
 let verificador2 = true;
-
-// clase para productos
-class producto {
-	constructor(codigo, nombre, medida, caja, precio, imagen) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.medida = medida;
-		this.caja = caja;
-		this.precio = precio;
-		this.imagen = imagen;
-	}
+let codViejo = 0;
+// base de datos de los porcelanatos
+const porcelanatos = JSON.parse(localStorage.getItem('porcelanatos'));
+console.log(porcelanatos);
+// cargo la eleccion del porcelanato desde el index
+const eleccion = JSON.parse(localStorage.getItem('eleccion'));
+console.log(eleccion);
+// si eleccion no es null cargo en el imput
+if (eleccion !== 0) {
+	$('#codigo').val(eleccion);
 }
-
-// carga de productos - creacion de objetos
-const carrara = new producto(1, 'Carrara', '60x120', 1.44, 5000, 'carrara.jpg');
-const onix = new producto(2, 'Onix', '60x60', 1.44, 4200, 'onix.jpg');
-const bauhaus = new producto(3, 'Bauhaus', '58x58', 1.35, 2800, 'bauhaus.jpg');
-// cargo mis productos en el array
-const porcelanatos = [carrara, onix, bauhaus];
-
 // funciones
-function imprimirPorc() {
-	porcelanatos.forEach((porc) => {
-		let galeria = document.getElementById('galeria');
-		let img = document.createElement('img');
-		img.setAttribute('src', `imagenes/${porc.imagen}`);
-		img.setAttribute('class', 'galeria__img');
-		let h3 = document.createElement('h3');
-		h3.setAttribute('class', 'galeria__titulo');
-		h3.textContent = `Porcelanato ${porc.nombre}`;
-		let parr = document.createElement('p');
-		parr.setAttribute('class', 'galeria__codigo');
-		parr.textContent = `Codigo: ${porc.codigo}`;
-		let div = document.createElement('div');
-		div.setAttribute('class', `Codigo`);
-		div.appendChild(img);
-		div.appendChild(h3);
-		div.appendChild(parr);
-		galeria.appendChild(div);
-	});
-}
-function ascendente(a, b) {
-	return a.codigo - b.codigo;
-}
-function descendente(a, b) {
-	return b.codigo - a.codigo;
-}
 function cantidad(metrosCuadrados) {
 	cantidadDeCajas = Math.ceil(metrosCuadrados / modelo.caja);
 	return cantidadDeCajas * modelo.caja;
@@ -82,79 +46,109 @@ function precioFinal(preciopiso, precioTotalPegamento, precioTotalPastina) {
 	return preciopiso + precioTotalPegamento + precioTotalPastina;
 }
 function mostrar() {
-	let numModelo = Number(document.getElementById('codigo').value);
-	let metrosCuadrados = Number(document.getElementById('cantidad').value);
-	if (numModelo != 1 && numModelo != 2 && numModelo != 3) {
-		alert('Usted a ingresado un numero de modelo incorrecto');
-		verificador1 = false;
-	} else {
-		verificador1 = true;
+	let numModelo = Number($('#codigo').val());
+	console.log(numModelo);
+	let metrosCuadrados = Number($('#cantidad').val());
+	let i = 0;
+	for (let value of porcelanatos) {
+		if (numModelo == value.codigo) {
+			modelo = value;
+			i++;
+			verificador1 = true;
+			console.log(modelo);
+		}
 	}
+	if (i == 0) {
+		verificador1 = false;
+	}
+	console.log(verificador1);
+	$('.datos__alerta').remove();
+	if (!verificador1) {
+		$('.datos__box:first-child').append(
+			`<p class="datos__alerta">Usted a ingresado un numero de modelo incorrecto.</p>`
+		);
+		verificador1 = false;
+	}
+	console.log(verificador1);
 	if (metrosCuadrados <= 0 || !metrosCuadrados) {
-		alert('Usted a ingresado una cantidad de metros cuadrado incorrecta');
+		$('.datos__box:last').append(
+			`<p class="datos__alerta">Usted a ingresado una cantidad de metros cuadrado incorrecta.</p>`
+		);
 		verificador2 = false;
 	} else {
 		verificador2 = true;
 	}
 	if (verificador1 && verificador2) {
-		switch (numModelo) {
-			case 1:
-				modelo = carrara;
-				break;
-			case 2:
-				modelo = onix;
-				break;
-			case 3:
-				modelo = bauhaus;
-				break;
-		}
 		let cantidadReal = cantidad(metrosCuadrados).toFixed(2);
 		let preciopiso = precioPorcelanato(cantidadReal);
 		let cantPegamentopastina = cantidadPegamentoPastina(cantidadReal);
-		let precioTotal = precioFinal(
-			preciopiso,
-			precioTotalPegamento(precioPegamento, cantPegamentopastina),
-			precioTotalPastina(precioPastina, cantPegamentopastina)
-		);
+		let precioPeg = precioTotalPegamento(precioPegamento, cantPegamentopastina);
+		let precioPast = precioTotalPastina(precioPastina, cantPegamentopastina);
+		let precioTotal = precioFinal(preciopiso, precioPeg, precioPast);
 		// modifico el DOM
-		let presupuesto = document.getElementById('presupuesto');
-		presupuesto.innerHTML = '';
-		let parrafo = document.createElement('p');
-		parrafo.textContent = `Por ${cantidadReal}m2 (${cantidadDeCajas} cajas) de porcelanato ${modelo.nombre} ${modelo.medida} + ${cantPegamentopastina} bolsas de pegamento de 30kg + ${cantPegamentopastina} bolsas de pastina de 1kg,  el precio total es de : $${precioTotal}`;
-		presupuesto.appendChild(parrafo);
-		parrafo.setAttribute('class', 'presupuesto__info');
-		let img = document.createElement('img');
-		presupuesto.appendChild(img);
-		img.setAttribute('src', `imagenes/${modelo.imagen}`);
-		img.setAttribute('class', 'presupuesto__img');
+		let presupuesto = $('.presupuesto');
+		$('.presupuesto__container').remove();
+		presupuesto.append(`
+			<div class="presupuesto__container">		
+				<p class="presupuesto__info">
+				Por ${cantidadReal}m2 (${cantidadDeCajas} cajas) de porcelanato ${modelo.nombre} ${modelo.medida} el precio es de: $${preciopiso}
+				</p>
+				<p class="presupuesto__info">Por ${cantPegamentopastina} bolsas de pegamento de 30kg el precio es de: $${precioPeg}</p>
+				<p class="presupuesto__info">Por ${cantPegamentopastina} bolsas de pastina de 1kg,  el precio es de: $${precioPast}</p>
+				<p class="presupuesto__info">El precio total es de : $${precioTotal}</p>
+				<img src="imagenes/${modelo.imagen}" class="presupuesto__img" />
+			</div>
+		`);
 		// cambio la seccion info
-		document.getElementById('porcNombre').textContent = ` ${modelo.nombre}`;
-		document.getElementById('porcMedida').textContent = `Medida: ${modelo.medida}`;
-		document.getElementById(
-			'porcCaja'
-		).textContent = `Metros cuadrados por caja: ${modelo.caja} m2`;
-		document.getElementById('porcPrecio').textContent = `Precio: $${modelo.precio}`;
+		let info = $('.info');
+		info.append('');
+		info.append(`
+		<div class="presupuesto__container">
+			<h1 class="info__nombre" id="porcNombre">Porcelanato ${modelo.nombre}</h1>
+			<h4 class="info__detalle" id="porcMedida">Medida: ${modelo.medida}</h4>
+			<h4 class="info__detalle" id="porcCaja">Metros cuadrados por caja: ${modelo.caja} m2</h4>
+			<h4 class="info__detalle" id="porcPrecio">Precio x m2: $${modelo.precio}</h4>
+		</div>
+		`);
+	} else {
+		$('.presupuesto__container').remove();
 	}
 }
-// ordeno los productos de menor a mayor precio y los muestro por consola
-console.log('Porcelanatos:');
-if (orden == 1) {
-	const ordenAscendente = porcelanatos.sort(ascendente);
-	for (const producto of ordenAscendente) {
-		console.log(
-			`Codigo: ${producto.codigo} - ${producto.nombre} ${producto.medida}: $${producto.precio}`
-		);
-	}
-} else {
-	const ordenDescendente = porcelanatos.sort(descendente);
-	for (const producto of ordenDescendente) {
-		console.log(`${producto.nombre} ${producto.medida}: $${producto.precio}`);
+function ingresaCod(value) {
+	for (modelo of porcelanatos) {
+		if (modelo.codigo == value) {
+			let presupuesto = $('.presupuesto');
+			$('.presupuesto__container').remove();
+			presupuesto.append(`
+				<div class="presupuesto__container">		
+					<img src="imagenes/${modelo.imagen}" class="presupuesto__img" />
+				</div>
+			`);
+			// cambio la seccion info
+			let info = $('.info');
+			info.append('');
+			info.append(`
+			<div class="presupuesto__container">
+				<h1 class="info__nombre" id="porcNombre">Porcelanato ${modelo.nombre}</h1>
+				<h4 class="info__detalle" id="porcMedida">Medida: ${modelo.medida}</h4>
+				<h4 class="info__detalle" id="porcCaja">Metros cuadrados por caja: ${modelo.caja} m2</h4>
+				<h4 class="info__detalle" id="porcPrecio">Precio x m2: $${modelo.precio}</h4>
+			</div>
+			`);
+		}
 	}
 }
-// creo una lista de productos de mi array
-imprimirPorc();
-// Agrego un evento
+function cambio() {
+	let inputMod = $('#codigo');
+	console.log(`el codigo nuevo es ${inputMod.val()} y el viejo es ${codViejo}`);
+	if (codViejo != inputMod.val()) {
+		inputMod.on('change', ingresaCod(inputMod.val()));
+		codViejo = inputMod.val();
+	}
+}
+// observo el input para tomar cualquier cambio en tiempo real para mostrar el porcelanato elegido
+setInterval(cambio, 300);
+// calculo el presupuesto cuando se le da click al boton calcular
 
-let btnPre = document.getElementById('btnPre');
-btnPre.addEventListener('click', mostrar);
-// muestro foto del modelo elegido
+let btnPre = $('#btnPre');
+btnPre.on('click', mostrar);
